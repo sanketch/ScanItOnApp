0c25eb9119d773783100164cb6f51f643bc2beb6
Sanket Chauhan <sanke93@gmail.com>
2013-10-23 03:04:07 -0700

    Update RegisterViewController.h

--- TutorialBase/RegisterViewController.h
+++ TutorialBase/RegisterViewController.h
@@ -1,11 +1,3 @@
-//
-//  RegisterViewController.h
-//  TutorialBase
-//
-//  Created by Antonio MG on 6/27/12.
-//  Copyright (c) 2012 AMG. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface RegisterViewController : UIViewController
